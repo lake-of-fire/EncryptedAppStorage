97261777c9ad4f43b90e4e20a821a2f91ba6ff7e
Alex Ehlke <alex.ehlke@gmail.com>
2023-11-19 21:09:55 -0500

    rename

--- Sources/EncryptedAppStorage/EncryptedAppStorage.swift
+++ Sources/EncryptedAppStorage/EncryptedAppStorage.swift
@@ -4,27 +4,26 @@ import KeychainAccess
 import SwiftUI
 
 @propertyWrapper
-public struct SecureStorage<Value: Codable>: DynamicProperty {
-
+public struct EncryptedAppStorage<Value: Codable>: DynamicProperty {
     private let publisher = PassthroughSubject<Value, Never>()
     @ObservedObject private var storage: KeychainStorage<Value>
-
+    
     public var wrappedValue: Value {
         get { storage.value }
-
+        
         nonmutating set {
             storage.value = newValue
             publisher.send(newValue)
         }
     }
-
+    
     init(wrappedValue: Value, _ key: String) {
         storage = KeychainStorage(
             defaultValue: wrappedValue,
             for: key
         )
     }
-
+    
     public var projectedValue: AnyPublisher<Value, Never> {
         publisher.eraseToAnyPublisher()
     }
